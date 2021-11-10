ff4f2a7c22597cc5d846b21331403bf4c70e0b3b
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-11-10 05:20:09 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3521,7 +3521,7 @@ keyboard.inline_keyboard = {
 {
 {text = 'تويت بالصوره', callback_data="/mlp4"},{text = 'لو خيروك بالصوره', callback_data="/mlp5"},
 },
-
+}
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
@@ -16065,7 +16065,7 @@ Msᴀɢ ~ #msgs
 🇪🇬𝗖𝗛 - »@eLmLoK0« ♔
 ]],
 [[
- 𝚄𝚂???? 𓄹𓄼 #username
+ 𝚄𝚂𝙴?? 𓄹𓄼 #username
  𝙸𝙳  𓄹𓄼 #id 
  𝚂𝚃𝙰 𓄹𓄼 #stast 
  𝙼𝚂𝙶𝚂𓄹𓄼 #msgs
@@ -18657,7 +18657,7 @@ datainline = {
 {{text =Mo,callback_data=data.sender_user_id_.."unmod"..userid},{text = 'ادمن ',callback_data=data.sender_user_id_.."unmod"..userid}},
 {{text =Sp,callback_data=data.sender_user_id_.."unSpecial"..userid},{text = 'مميز ',callback_data=data.sender_user_id_.."unSpecial"..userid}},
 {{text = '♔ 𝙱𝙰𝙲𝙺',callback_data=data.sender_user_id_.."Bbk"..userid}},
-{{text = '♔ 𝚂𝙾??𝚁𝙲𝙴 𝙴𝙻𝙼𝙻𝙾𝙺', url="t.me/eLmLoK0"}},
+{{text = '♔ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙴𝙻𝙼𝙻𝙾𝙺', url="t.me/eLmLoK0"}},
 }
 elseif BasicConstructor(data) then
 datainline = {
